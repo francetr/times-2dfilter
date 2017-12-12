68d8e600fc7af9e47ff64f8edebdd3890205c338
francetr <tristanfrances@yahoo.fr>
2017-12-12 15:22:44 +0100

    utilisation de pipe

--- examples/mean_test.js
+++ examples/mean_test.js
@@ -12,3 +12,28 @@ let view = T.view(img.getRaster());
 win.addView(view);
 // add window to the DOM
 win.addToDOM("workspace");
+
+
+/*
+* Apply the mean filter on the image
+*/
+let kernel="1 1 1\n1 1 1\n1 1 1";
+kernel = splitKernel(kernel);
+
+let workflow = T.pipe(
+  meanFilter(kernel.kernel),
+  T.view
+);
+let view2 = workflow(img.getRaster());
+
+// let newImg = new T.Image("uint8", 360, 288);
+// let applyMean = meanFilter(kernel.kernel) (img.raster);
+// newImg.setPixels(applyMean.pixelData);
+
+let win2 = new T.Window('Mean Filter');
+// create a new view for the window
+// let view2 = T.view(newImg.getRaster());
+// add view to the window
+win2.addView(view2);
+win2.addToDOM("workspace");
+// add window to the DOM
