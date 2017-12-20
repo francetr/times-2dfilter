f6ec89bbfe9f6a7bddc116056fe2a47b8dde6bfb
francetr <tristanfrances@yahoo.fr>
2017-12-20 18:00:53 +0100

    fonction pour creer fichier csv des benchmark

--- examples/mean_test.js
+++ examples/mean_test.js
@@ -58,8 +58,78 @@ img.setPixels(result.pixelData);
 let win2 = new T.Window('Mean Filter');
 let view2 = T.view(img.getRaster())
 // create a new view for the window
-// let view2 = T.view(newImg.getRaster());
+
 // add view to the window
 win2.addView(view2);
 win2.addToDOM("workspace");
 // add window to the DOM
+
+// BENCHMARK
+// let img_tmp = new T.Image("uint8", img.width, img.height);
+// let win_tmp = new T.Window('Benchmark');
+// let view_tmp = T.view(img_tmp.getRaster());
+// win_tmp.addView(view_tmp);
+// win_tmp.addToDOM("workspace");
+
+// let res_uint8 = [];
+let r =0
+
+
+"360", "280"
+const newImages=(type, width, height)=>{
+  for (let i = 0; i < width*1000; i+=100) {
+    for (let j = 0; i < height*1000; j+=100) {
+      if (type === "uint8") {
+        let img = new T.Image("uint8", 360, 288);
+      };
+      img.setPixels(boats_pixels);
+
+    };
+  };
+
+
+}
+
+
+const benchmark = (img_ref, st) =>{
+  let r = 0;
+  while ( r <10) {
+    let start = new Date().getMilliseconds();
+    let img_res = meanFilter(kernel)(img_ref, true);
+    // img_tmp.setPixels(img_res.pixelData);
+    let stop = new Date().getMilliseconds();
+    let bench = stop-start;
+    if (bench <0) {
+      bench=0;
+    }
+    bench =bench.toString();
+    st = st.concat(bench).concat(",");
+    r++;
+  }
+  return st;
+}
+
+/**
+ * Function to download a csv file
+ *
+ * @return {TRaster} - Filtered Image by using the convolution function
+ *
+ *  This function proceed a benchmark and then add a download action in the
+ *  html page to download the result of the benchmark in a csv file 
+ *
+ * @author Gabor Szabo
+ */
+function download_csv() {
+
+    let csv = benchmark(img, "");
+
+    console.log(csv);
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'benchmark.csv';
+    document.getElementById('container').appendChild(hiddenElement);
+    hiddenElement.click();
+}
+
+// console.log(benchmark(img, " "));
